8723e6f915fb7ab86b304d4b4ed42329733b0ec1
mohamdii <m.hamdy97@gmail.com>
2023-02-16 18:05:22 -0800

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,13 +6,13 @@
 
 int main(void)
 {
-	char a = 'A';
+	char a = "A";
 	int b = 2;
 	long int c =4;
 	long long int d =9;
 	float e =2.4;
 
-	printf("Size of a char:%c\n",sizeof(a));
+	printf("Size of a char:%d\n",sizeof(a));
 	printf("Size of an int:%d\n",sizeof(b));
 	printf("Size of a long int:%d\n",sizeof(c));
 	printf("Size of a long long int:%d\n",sizeof(d));
