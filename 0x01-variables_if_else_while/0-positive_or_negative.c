5de79c515478d1afd8491f9a28808234b4bfe7c6
mohamdii <m.hamdy97@gmail.com>
2023-02-17 14:59:27 -0800

    0

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * This code is for testing 0 value 
+ * 
+ * Returing a value
+ */
+int main(void)
+{
+	char n;
+	
+	if (n == 0){
+		printf("%c:is zero",n);
+	} else if (n > 0) {
+		printf("%c:is positive",n);
+	} else {
+		printf("%c:is negative",n);
+	}
+	return (0);
+}
