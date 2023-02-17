844ce7106f24f7a7566f1a7ad0fe1f5c7e9fe406
mohamdii <m.hamdy97@gmail.com>
2023-02-16 18:41:05 -0800

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,24 +4,22 @@
  *this code is made 
  *for measuring sizes of funcionts
  */
-
 int main(void)
 {
-	/* creating variables  */
+
+/* creating variables  */
+char a = "A";
+int b =  2;
+long int c = 4;
+long long int d = 9;
+float e = 2.4;
 	
-	char a = "A";
-	int b =  2;
-	long int c = 4;
-	long long int d = 9;
-	float e = 2.4;
-	
-	/* Printing size of the variables */
-	
-	printf("Size of a char: %d byte(s)\n",sizeof(a));
-	printf("Size of an int: %d byte(s)\n",sizeof(b));
-	printf("Size of a long int: %d byte(s)\n",sizeof(c));
-	printf("Size of a long long int: %d byte(s)\n",sizeof(d));
-	printf("Size of a float: %d byte(s)\n",sizeof(e));
-	return (0);
+/* Printing size of the variables */	
+printf("Size of a char: %d byte(s)\n",sizeof(a));
+printf("Size of an int: %d byte(s)\n",sizeof(b));
+printf("Size of a long int: %d byte(s)\n",sizeof(c));
+printf("Size of a long long int: %d byte(s)\n",sizeof(d));
+printf("Size of a float: %d byte(s)\n",sizeof(e));
+return (0);
 }
 
