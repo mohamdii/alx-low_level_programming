a08c646ec2f222e570f912db2ac2b8315ee8e696
mohamdii <m.hamdy97@gmail.com>
2023-02-16 18:52:24 -0800

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,21 +6,13 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-{
-
-/* creating variables  */
-char a = "A";
-int b =  2;
-long int c = 4;
-long long int d = 9;
-float e = 2.4;
+{	
 	
-/* Printing size of the variables */	
-printf("Size of a char: %d byte(s)\n", sizeof(a));
-printf("Size of an int: %d byte(s)\n", sizeof(b));
-printf("Size of a long int: %d byte(s)\n", sizeof(c));
-printf("Size of a long long int: %d byte(s)\n", sizeof(d));
-printf("Size of a float: %d byte(s)\n", sizeof(e));
+printf("Size of a char: %d byte(s)\n", sizeof(char));
+printf("Size of an int: %d byte(s)\n", sizeof(int));
+printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+printf("Size of a float: %d byte(s)\n", sizeof(float));
 return (0);
 }
 
