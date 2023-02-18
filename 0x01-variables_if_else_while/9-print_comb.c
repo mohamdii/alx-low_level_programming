a351ea97ae6c6d8c5136f0b91848d9e2bc77d181
mohamdii <m.hamdy97@gmail.com>
2023-02-17 19:01:14 -0800

    9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 100; i++)
+	{
+		putchar((i % 10) + '0');
+		if (i == 9)
+			continue;
+
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+
+	return (0);
+}
