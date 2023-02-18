ad065f7c7485cb3635a45e63d00ce941144d54fd
mohamdii <m.hamdy97@gmail.com>
2023-02-17 17:14:05 -0800

    3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch;
+	char zh;
+	for (ch = 'a'; ch >='z'; ch++)
+	{
+		putchar(ch);
+	}
+	for (zh = 'A'; zh >= 'Z'; zh++)
+	{
+		putchar(zh);
+	}
+	putchar('\n');
+	return (0);
+}
