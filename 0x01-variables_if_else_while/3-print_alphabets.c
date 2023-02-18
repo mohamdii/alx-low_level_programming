82b450ea77bd908da38676fe303adce281fbaae8
mohamdii <m.hamdy97@gmail.com>
2023-02-17 17:20:16 -0800

    3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,11 +10,11 @@ int main(void)
 {
 	char ch;
 	char zh;
-	for (ch = 'a'; ch >='z'; ch++)
+	for (ch = 'a'; ch <='z'; ch++)
 	{
 		putchar(ch);
 	}
-	for (zh = 'A'; zh >= 'Z'; zh++)
+	for (zh = 'A'; zh <= 'Z'; zh++)
 	{
 		putchar(zh);
 	}
