7aec59fc57630f9ddb3e209dfca8b600b578ef1e
mohamdii <m.hamdy97@gmail.com>
2023-02-20 12:11:32 -0800

    last

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -18,19 +18,20 @@ int main (void)
 			{
 				for (l = 0; l <= 9; l++)
 				{
-					if (l == 0)
+					if ( l == 0 && k == 0 && i == 0 && j == 0)
 						continue;
 					putchar((i) + '0');
 					putchar((j) + '0');
 					putchar(' ');
 					putchar((k) + '0');
 					putchar((l) + '0');
-					if (i == 9 && j == 8 && k == 9 && l == 9)
+					if (i == 9 && j == 9 && k == 9 && l == 9)
 					{
 					}
-					else 
+					else
 					{
 					putchar(',');
+					putchar(' ');
 					}
 				}
 			}
