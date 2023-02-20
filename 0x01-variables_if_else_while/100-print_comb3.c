0ee74bef5709d29d299263940428847c740a96d5
mohamdii <m.hamdy97@gmail.com>
2023-02-20 09:28:25 -0800

    10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,13 +8,13 @@
 
 int main(void)
 {
-	int i,j;
+	int i, j;
 
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
-			if ( i >= j)
+			if (i >= j)
 				continue;
 			putchar((i) + '0');
 			putchar((j) + '0');
