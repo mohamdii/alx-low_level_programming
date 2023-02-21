637672f1e867b24a55a2548e9c060fb91f8ef3be
mohamdii <m.hamdy97@gmail.com>
2023-02-21 12:54:01 -0800

    3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *int _islower(int c); - Returns 1 if char is lower
+ */
+int _islower(int c)
+{
+	int c;
+
+	islower(c);
+	return(1);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,3 +9,4 @@ void _putchar();
 void print_alphabet(void);
 /*prints the alphabets 10 times */
 void print_alphabet_x10(void);
+int _islower(int c);
