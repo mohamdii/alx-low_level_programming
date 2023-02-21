fad28fb0f16bc523f931be0da6dabeb51f5559f2
mohamdii <m.hamdy97@gmail.com>
2023-02-21 12:00:10 -0800

    1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,16 +2,9 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
- * Return: Always 0.
- */
-
-/**
  *  print_alphabet function 
  *
  *  printslower case alphabets 
- *  returns void
  */
 void print_alphabet(void)
 {
