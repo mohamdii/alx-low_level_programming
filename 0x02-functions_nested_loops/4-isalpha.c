a338316a868de5f1f6b8b6a03165b6edb7aa981e
mohamdii <m.hamdy97@gmail.com>
2023-02-21 13:30:04 -0800

    4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * _isalpha - checks alphabets
+ * @c: The number to be checked
+ *
+ * Return: 1 if its alphabet. 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if (_isalpha(c))
+	{
+		return (1);
+	}
+	else 
+	{
+		return (0);
+	}
+}
