e41afe23c2b7fe675a52e200e0fac2c92b0e2f1a
mohamdii <m.hamdy97@gmail.com>
2023-02-21 14:14:05 -0800

    5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,17 +10,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		return (1);
 		putchar('+');
+		return (1);
 	}
 	else if (n < 0)
 	{
-		printf("-1");
+		putchar('-');
 		return (-1);
 	}
 	else
 	{
-		printf("0");
+		putchar('0');
 		return (0);
 	}
 }
