b4fa3e3a3b37ff9162a4ac3181ba7c798e47d219
mohamdii <m.hamdy97@gmail.com>
2023-02-21 17:27:30 -0800

    6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 /**
  * int _abs - to print absolute value of a variable
+ * @x: Checks the number
  *
- * 
- *
+ * @z: Returns the absolute value of input x 
  */
 
 int _abs(int x)
