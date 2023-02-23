d3d8ac3f463365b74df588351e8d65d8d15ed84b
mohamdii <m.hamdy97@gmail.com>
2023-02-23 15:44:05 -0800

    7

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,16 +13,19 @@ int main(void)
 	{
 		if (i % 15 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
+			putchar(' ');
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
+			putchar(' ');
 		}
 
 		else if (i % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
+			putchar(' ');
 		}
 		else
 			printf("%d ", i);
