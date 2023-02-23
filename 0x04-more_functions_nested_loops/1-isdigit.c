3aa5a8e6d8c11eef104476c6917fc34faf15e977
mohamdii <m.hamdy97@gmail.com>
2023-02-23 10:26:46 -0800

    2

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,9 +3,8 @@
 /** _isdigit - function to check digit
  * @c: check input of charactar
  *
- * Return: 1 if c is digit. 0 otherwise.
+ * Return: 1 if c is digit. 0 otherwise
  */
-
 int _isdigit(int c)
 {
 	if (isdigit(c))
