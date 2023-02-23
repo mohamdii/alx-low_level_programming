6b39e3d499b565bf737d982dd643aa1ede0762ec
mohamdii <m.hamdy97@gmail.com>
2023-02-23 11:54:25 -0800

    4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,13 +3,14 @@
 /**
  * print_most_number - prints 1-9 except 2 & 4
  */
+
 void print_most_numbers(void)
 {
 	int i;
 
 	for (i = 0; i <= 9; i++)
 	{
-		if ( i == 2 || i == 4)
+		if (i == 2 || i == 4)
 			continue;
 		_putchar(i + '0');
 	}
