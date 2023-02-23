0aa58522453e21096fba7457010c59933e01b9aa
mohamdii <m.hamdy97@gmail.com>
2023-02-23 09:55:04 -0800

    2

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - functions checks upper ulphapets
+ * @c: check alphabet
+ *
+ * Return: 1 if upper. 0 other wise
+ */
+
+int _isupper(int c)
+{
+	if (isupper(c))
+		return (1);
+	else
+		return (0);
+}
