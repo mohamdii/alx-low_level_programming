62a997295bca5423acccb65ac02b3620639f47e2
mohamdii <m.hamdy97@gmail.com>
2023-02-23 10:35:55 -0800

    2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - function of multiplication
+ * @a: checks a 
+ *
+ * @b: checks b
+ * Return: result of multiplication
+ */
+
+int mul(int a, int b)
+{
+	int z;
+	z = a * b;
+	return (z);
+}
