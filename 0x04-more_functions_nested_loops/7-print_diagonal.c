95d4d12724059b14156d71fe5e2d8556dbea6bbf
mohamdii <m.hamdy97@gmail.com>
2023-02-23 13:59:45 -0800

    7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_diagonal - function to print slashes diagonally
+ * @n: number of spaces
+ */
+void print_diagonal(int n)
+{
+	int i, z;
+
+	if ( n > 0)
+	{
+		for (i = 1; i <= n; i++)
+		{
+			for (z = 1; z <= i; z++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
