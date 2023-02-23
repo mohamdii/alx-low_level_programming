d567deb4d00bb20feb3793b033428f52e422d7f8
mohamdii <m.hamdy97@gmail.com>
2023-02-23 11:18:45 -0800

    2

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,7 +1,9 @@
-#include <stdio.h>
-#include <ctype.h>
-
+#ifndef MAIN_h
+#define MAIN_h
+void _putchar(char x);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
+
+#endif 
