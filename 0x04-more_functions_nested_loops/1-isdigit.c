1d215f2ad2b78e4c387fe455b56dd53c9d0cdb91
mohamdii <m.hamdy97@gmail.com>
2023-02-23 10:18:21 -0800

    1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/** _isdigit - function to check deget
+ * @c: check input
+ *
+ * Return: 1 if c is digit. 0 otherwise.
+ */
+int _isdigit(int c)
+{
+	if (isdigit(c))
+		return (1);
+	else 
+		return (0);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,5 @@
 #include <stdio.h>
 #include <ctype.h>
+
 int _isupper(int c);
+int _isdigit(int c);
