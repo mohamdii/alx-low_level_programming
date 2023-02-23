50882c296e0346781c41420137c13412b7b1953d
mohamdii <m.hamdy97@gmail.com>
2023-02-23 14:46:03 -0800

    9

--- 0x04-more_functions_nested_loops/main.c
+++ 0x04-more_functions_nested_loops/main.c
@@ -6,11 +6,9 @@
  * Return: Always 0.
  */
 int main(void)
-
 {
-    print_diagonal(0);
-    print_diagonal(2);
-    print_diagonal(10);
-    print_diagonal(-4);
+    print_square(2);
+    print_square(10);
+    print_square(0);
     return (0);
 }
