beb4bb03827abd97160f0c1130bbc13f40fc558a
mohamdii <m.hamdy97@gmail.com>
2023-02-23 11:44:40 -0800

    5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * more_number - function to print numbers
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (j = 0; j <= 10; j++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			_putchar(i + '0');
+		}
+		_putchar('\n');
+	}
+}
