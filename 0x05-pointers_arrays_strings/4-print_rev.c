31e972ef72edaa257ab868a52926cb8b6b475b49
mohamdii <m.hamdy97@gmail.com>
2023-02-27 14:52:33 -0800

    3

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * print_rev - prints function in reverse
+ * @s: input variable
+ */
+
+void print_rev(char *s)
+{
+
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,10 +2,12 @@
 #define MAIN_H
 
 #include <stdio.h>
+
 void _putchar(char x);
 int _strlen(char *s);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 void _puts(char *str);
+void print_rev(char *s);
 
 #endif 
