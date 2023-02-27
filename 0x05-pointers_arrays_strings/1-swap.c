f2a7794af3449644346a35be644cefc78619f6b8
mohamdii <m.hamdy97@gmail.com>
2023-02-27 12:41:36 -0800

    3

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * swap_int - function that swaps the values
+ * @a: checks first number
+ * @b: checks second number
+ */
+void swap_int(int *a, int *b)
+{
+	*a = *b;
+	*b = *a;
+}
