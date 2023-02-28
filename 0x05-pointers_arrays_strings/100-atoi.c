185cc0617f717f08d274614bc5b5a867ace65c8b
mohamdii <m.hamdy97@gmail.com>
2023-02-28 15:27:59 -0800

    10

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ *
+ *
+ */
+int _atoi(char *s)
+{
+	int i, res;
+
+	res = 0;
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 48 && s[i] <= 57)
+		{
+			res = res * 10 + s[i] - '0';
+		}
+		if (s[i] == ',')
+			break;
+		i++;
+	}
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == '-')
+			res = -res;
+		i++;
+	}
+	return (res);
+}
