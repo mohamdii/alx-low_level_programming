915756c6ffeefed290a3ab813bf005f5b28afbbd
mohamedii <m.hamdy97@gmail.com>
2023-03-07 01:00:21 +0000

    5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,12 +10,20 @@
 
 char *_strstr(char *haystack, char *needle)
 {
-	int i = 0;
+	int i = 0, j;
 
-	while (*(haystack + i) != '\0')
+	while (*(needle + i) != '\0')
 	{
-		if (*(haystack + i) == needle[0])
-			return((needle + 0));
+		j = 0;
+		while (*(haystack + j) != '\0')
+		{
+			if (*(haystack + j) == *(needle + i))
+				break;
+			else
+				return (needle + i);
+			j++;
+
+		}
 		i++;
 	}
 	return ('\0');
