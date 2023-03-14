e70313106170155b413e558ec594ff4dc6c00b18
mohamedii <m.hamdy97@gmail.com>
2023-03-14 19:04:36 +0000

    1

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -4,7 +4,7 @@
  * create_array - creates an array with specific chars
  * @size: size of the array
  * @c: the specific charactar
- * 
+ *
  * Return: char
  */
 
@@ -14,7 +14,7 @@ char *create_array(unsigned int size, char c)
 	unsigned int i = 0;
 
 	if (size == 0)
-		return('\0');
+		return ('\0');
 	p = malloc(sizeof(c) * size);
 	if (p == NULL)
 		return (NULL);
