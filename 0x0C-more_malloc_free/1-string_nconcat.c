acb1621ac70732900eab4dc600af0a79f5d9c356
mohamdii <m.hamdy97@gmai.com>
2023-03-17 01:29:18 +0000

    1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,6 +15,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	int i;
 	unsigned int j;
 
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	i = 0;
 	while (*(s1 + i) != '\0')
 	{
