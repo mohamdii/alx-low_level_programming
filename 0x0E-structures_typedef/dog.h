36964f47d1b56159a9b88f2de38cb344a58c875b
mohamdii <m.hamdy97@gmai.com>
2023-03-20 21:34:36 +0000

    1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,16 +8,17 @@
  *
  * Description: this is what could possibly be stored
  */
-/**
- * dog_t - Typedef for struct dog
- */
-typedef struct dog dog_t;
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
