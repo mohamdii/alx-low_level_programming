2cc22912ffaeaf6b41f8400597d25a78489d0614
mohamdii <m.hamdy97@gmai.com>
2023-03-23 21:10:55 +0000

    1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,6 +1,7 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
 #include <stdio.h>
+#include <stdlib.h>
 /**
  * print_numbers - function that prints numbers follower by new line
  * @separator: string to be put bet numbers
@@ -12,6 +13,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list ptr;
 	unsigned int i;
 
+	if (separator == NULL)
+		return;
 	separator = ",";
 	va_start(ptr, n);
 	for (i = 0; i < n; i++)
