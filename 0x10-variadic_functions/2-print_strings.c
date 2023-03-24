f894ab95e0c4eb33a88a8798498d1d68945399ac
mohamdii <m.hamdy97@gmai.com>
2023-03-24 01:51:54 +0000

    2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -26,6 +26,6 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (separator && n - 1 > i)
 			printf("%s", separator);
 	}
-	printf("\n");	
+	printf("\n");
 	va_end(ptr);
 }
