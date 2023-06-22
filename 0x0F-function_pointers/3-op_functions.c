7f42ae7cc41bf963afa725a790dd56d4bad90d4d
mohamdii <m.hamdy97@gmai.com>
2023-06-22 02:56:31 +0000

    3

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,21 +1,55 @@
 #include "calc.h"
-
+/**
+ * op_add - returns product of two numbers
+ * @a: int parameter
+ * @b: int parameter
+ *
+ * Return: (@a * @b)
+ */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
+/**
+ * op_sub - returns product of two numbers
+ * @a: int parameter
+ * @b: int parameter
+ *
+ * Return: (@a * @b)
+ */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
+/**
+ * op_mul - returns product of two numbers
+ * @a: int parameter
+ * @b: int parameter
+ *
+ * Return: (@a * @b)
+ */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
+/**
+ * op_div - returns product of two numbers
+ * @a: int parameter
+ * @b: int parameter
+ *
+ * Return: (@a * @b)
+ */
 int op_div(int a, int b)
 {
 	return (a / b);
 }
+/**
+ * op_mod - returns product of two numbers
+ * @a: int parameter
+ * @b: int parameter
+ *
+ * Return: (@a * @b)
+ */
 int op_mod(int a, int b)
 {
 	return (a % b);
