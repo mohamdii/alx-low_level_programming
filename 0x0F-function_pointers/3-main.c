f0e39889f54c2d65255eff7c58aa26be4759d2ad
mohamdii <m.hamdy97@gmai.com>
2023-06-22 02:44:45 +0000

    1

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -6,16 +6,17 @@ int main(int argc, char *argv[])
 
 {
 	int num1, num2, result;
-	char operator;
+	char op;
 	int (*f)(int, int);
 	
 	if (argc == 0)
 		printf("please insert a number");	
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
+	op = *argv[2];
 	f = get_op_func(argv[2]);
 	result = f(num1, num2);
-	if ((operator == '/' || operator == '%') && num2 == 0)
+	if ((op == '/' || op == '%') && num2 == 0)
 	{
 		printf("Error\n");
 		exit(100);
