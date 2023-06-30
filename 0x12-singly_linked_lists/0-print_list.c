6ed4bbbdd37ff7c1c693e2c035dc6118da22a865
mohamdii <m.hamdy97@gmai.com>
2023-06-30 16:23:44 +0000

    3

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,9 +1,8 @@
 #include "lists.h"
 #include <stdio.h>
-
 size_t print_list(const list_t *h)
 {
-	unsigned size_t count = 0;
+	size_t count = 0;
 
 	while (h != NULL)
 	{
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -4,7 +4,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-size_t print_list(const list_t *h);
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -20,5 +19,6 @@ typedef struct list_s
     struct list_s *next;
 } list_t;
 
+size_t print_list(const list_t *h);
 
 #endif
