186db869319446be041229f41153cf61f44fbc15
mohamdii <m.hamdy97@gmai.com>
2023-06-30 18:43:34 +0000

    fixing betty for task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,12 +1,20 @@
 #include "lists.h"
 #include <stdio.h>
+
+/**
+ * print_list - function that prints whats in the list
+ * @h: a pointer struct
+ * Return: size of return
+ */
+
 size_t print_list(const list_t *h)
 {
 	size_t count = 0;
 
 	while (h != NULL)
 	{
-		if (h->str != NULL) {
+		if (h->str != NULL)
+		{
 			printf("[%d] %s\n", h->len, h->str);
 		}
 		else
