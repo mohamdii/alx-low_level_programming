bac82dc54120c5a9319294283a1023558e4f5101
mohamdii <m.hamdy97@gmail.com>
2024-01-11 13:11:13 +0200

    1

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,6 +1,12 @@
 #include "lists.h"
 #include <stdlib.h>
 #include <stdio.h>
+/**
+ * dlistint_len - function that counts elements
+ *
+ * @h: is a next pointer
+ * Return: size_t type counter
+ */
 size_t dlistint_len(const dlistint_t *h)
 {
 	size_t i = 0;
