7bd2da719bd4788e5aa2197d132da251b2592a13
mohamdii <m.hamdy97@gmail.com>
2024-01-11 15:00:29 +0200

    2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,5 +1,10 @@
 #include "lists.h"
-
+/**
+ * dlistint_t - function that create a node
+ * @head: pointers points to head
+ * @n: constant value
+ * Return: struct
+ */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *newNode = (dlistint_t *)malloc(sizeof(dlistint_t));
