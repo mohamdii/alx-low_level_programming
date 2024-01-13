0f18cfdd76dd58b8e2aaf3b35679546a7a1de0e0
mohamdii <m.hamdy97@gmail.com>
2024-01-13 18:55:54 +0200

    5

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *point;
+	unsigned int count = 0;
+	
+	point = head;
+	while(point->next != NULL)
+	{
+		if (count == index)
+			break;
+		point = point->next;
+		count++;
+	}
+	
+	return point;
+}
