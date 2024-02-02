aceff08793233086ddd4152be8a1536491dfecba
mohamdii <m.hamdy97@gmail.com>
2024-02-02 05:35:08 +0200

    2

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,7 +1,7 @@
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned long int hash = djb2_hash(*key);
+	unsigned long int hash = djb2_hash((unsigned char *)key);
 
 	unsigned long int index = hash_value % size;
 
