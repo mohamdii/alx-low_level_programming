980fdef3d8b9b891ae407db26a3adeef7c138f3e
mohamdii <m.hamdy97@gmail.com>
2024-02-02 11:03:22 +0200

    3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -8,7 +8,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	int index;
 	if (new_node == NULL)
 		return 0;
-	if (strcmp(key, "") == 1)
+	if (strcmp(key, "") == 0)
 		return 0;
 	index = key_index((const unsigned char *)key, ht->size);
 	new_node->next = ht->array[index];
