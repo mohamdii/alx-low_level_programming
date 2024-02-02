c465fbe3c6f1286d5608e0118a201fd5734e273c
mohamdii <m.hamdy97@gmail.com>
2024-02-02 05:37:39 +0200

    2

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,4 +1,10 @@
-
+/**
+ * key_index - function that finds the key
+ *
+ * @key: the key
+ * @size: size of array
+ * return: int
+ */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int hash = djb2_hash((unsigned char *)key);
