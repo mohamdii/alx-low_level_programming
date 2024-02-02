0c9e39dcaa2febee8dadde668548075a48ddca6a
mohamdii <m.hamdy97@gmail.com>
2024-02-02 12:33:49 +0200

    4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "hash_tables.h"
+#include <string.h>
 /**
  * hash_table_get - function that retrieves a value associated with a key
  * @ht: hash table
@@ -10,10 +11,16 @@
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	int index;
+	hash_node_t *p;
 
-	index = key_index((unsigned char *)key, ht->size);
-	if (ht->array[index]->value == NULL || ht->array[index]->key == NULL)
+	if (ht == NULL || key == NULL)
 		return NULL;
-	return ht->array[index]->value;
+	index = key_index((const unsigned char *)key, ht->size);
+	p = ht->array[index];
+	for(;p != NULL; p = p->next)
+	{
+		if (strcmp(p->key, key) == 0)
+			return p->value;
+	}
+	return NULL;
 }
-
