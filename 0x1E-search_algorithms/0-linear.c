3558e6cf5cbc8e74539fcb770f75f382a17fcae2
mohamdii <m.hamdy97@gmail.com>
2024-05-07 08:05:55 +0300

    simple test

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,25 @@
+#include "search_algos.h"
+/**
+ * linear_search - function that does linear search
+ * @array: array to be searched
+ * @size: size of the array
+ * @value: is the value we lookin for
+ * 
+ * Return: an int
+*/
+
+int linear_search(int *array, size_t size, int value)
+{
+    size_t i = 0;
+    if (array == NULL)
+        return -1;
+
+    for(i = 0; i < size; i++)
+    {
+        if (array[i] == value)
+        {
+            return i;
+        }
+    }
+    return -1;
+}
